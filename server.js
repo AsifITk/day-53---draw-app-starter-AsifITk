8b5f5852e4ea8825e85b0632bf1d37f0f1ba8658
AsifITk <asifkhanit10@gmail.com>
2022-09-10 02:58:58 +0600

    final

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require("express");
+var cors = require('cors')
+const { Server } = require("socket.io");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(cors())
+
+const httpServer = app.listen(process.env.PORT || 8000, () => {
+  console.log("listening...")
+});
+const io = new Server(httpServer)
+let historyArr = []
+io.on("connection", (socket) => {
+  console.log("Client Connected", socket.id)
+  socket.on("chat-message", (data) => {
+    console.log(data + " by " + socket.id)
+    socket.broadcast.emit("new-message", `${data} by ${socket.id}`)
+  })
+  socket.on("drawnByClient", (data) => {
+    historyArr.push(data)
+    socket.broadcast.emit("broadcastDrawing", historyArr)
+  })
+  socket.emit("initializeBroadcast", [historyArr, [Math.ceil(Math.random() * 265), Math.ceil(Math.random() * 265), Math.ceil(Math.random() * 265)]])
+  socket.on("clearBtn", () => {
+    historyArr = []
+    io.emit("clearBoard")
+  })
+})
